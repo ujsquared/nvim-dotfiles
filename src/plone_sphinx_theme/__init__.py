a9ac0594389626b932e2b8544e1b879ac42d2716
Steve Piercy <web@stevepiercy.com>
2024-09-19 02:03:16 -0700

    Back to development: 0.0.12

--- src/plone_sphinx_theme/__init__.py
+++ src/plone_sphinx_theme/__init__.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from sphinx.application import Sphinx
 from sphinx.util import logging
 
-__version__ = "0.0.11"
+__version__ = "0.0.12.dev0"
 
 from sphinx_book_theme import update_context_with_repository_info, update_sourcename
 
